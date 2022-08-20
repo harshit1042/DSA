b716d7cc71614aa31c04c182caee560d728d6d09
harshit1042 <105489003+harshit1042@users.noreply.github.com>
2022-08-20 21:53:22 +0530

    Add files via upload

---
+++ linkedlist.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node
+{
+    int data;
+    struct Node *next;
+};
+
+void linkedlisttraversal(struct Node *ptr)
+{
+    while (ptr != NULL)
+    {
+        printf("Element :%d\n", ptr->data);
+        ptr = ptr->next;
+    }
+}
+
+int main()
+{
+    struct Node *head;
+    struct Node *second;
+    struct Node *third;
+
+    // Allocate memory for nodes in the linked list in HEAP
+    head = (struct Node *)malloc(sizeof(struct Node));
+    second = (struct Node *)malloc(sizeof(struct Node));
+    third = (struct Node *)malloc(sizeof(struct Node));
+
+    // Link first and second nodes
+    head->data = 7;
+    head->next = second;
+    // Link second and third nodes
+    second->data = 11;
+    second->next = third;
+    // Terminate the list at the third node
+    third->data = 6;
+    third->next = NULL;
+
+    linkedlisttraversal(head);
+    return 0;
+}
