2d41444f497a22c1e6cea26a735952b2af81adab
harshit1042 <105489003+harshit1042@users.noreply.github.com>
2022-08-31 17:38:28 +0530

    Add files via upload

---
+++ bubble_sort.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+
+void printArray(int *A, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", A[i]);
+    }
+    printf("\n");
+}
+
+void bubbleSort(int *A, int n)
+{
+    int temp;
+    int isSorted = 0;
+    for (int i = 0; i < n - 1; i++)
+    {
+        printf("Working on pass number %d\n", i + 1);
+        for (int j = 0; j < n - 1 - i; j++)
+        {
+            if (A[j] > A[j + 1])
+            {
+                temp = A[j];
+                A[j] = A[j + 1];
+                A[j + 1] = temp;
+            }
+        }
+    }
+}
+
+void bubbleSortAdaptive(int *A, int n)
+{
+    int temp;
+    int isSorted;
+    for (int i = 0; i < n - 1; i++)
+    {
+        printf("Working on pass number %d\n", i + 1);
+        isSorted = 1;
+        for (int j = 0; j < n - 1 - i; j++)
+        {
+            if (A[j] > A[j + 1])
+            {
+                temp = A[j];
+                A[j] = A[j + 1];
+                A[j + 1] = temp;
+                isSorted = 0;
+            }
+        }
+        if (isSorted)
+        {
+            return;
+        }
+    }
+}
+
+int main()
+{
+    int A[] = {1, 65, 96, 66, 77, 93};
+    int n = 6;
+    printArray(A, n);
+    bubbleSort(A, n);
+    printArray(A, n);
+}
